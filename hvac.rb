36889bd924cbf5f9b77a80b949d64332b3a94b4e
michaeldavie <michael.davie@gmail.com>
2015-12-02 21:54:29 -0500

    Updated logic
    Updated decision logic and added methods

--- hvac.rb
+++ hvac.rb
@@ -1,41 +1,72 @@
 #!/usr/bin/env ruby
 
+# This script is designed to control both heating and cooling with a dual-fuel 
+# heating system (heat pump and boiler) and a heat punmp for cooling. The Nest 
+# controls the boiler directly, and the script passes commands for the heat 
+# pump from the Nest to the Sensibo
+
 require 'nest_thermostat'
 require 'sensibo'
 
-nest = NestThermostat::Nest.new(email: 'user@email.com', password: 'mypassword', temperature_scale: :celsius)
-sensibo = Sensibo::System.new('myapikey')
+#***** Initialization
 
-sensiboOn = sensibo.pods[0].on
-sensiboMode = sensibo.pods[0].mode
-sensiboFan = 'auto'
-sensiboTemp = sensibo.pods[0].targetTemp
+@nest = NestThermostat::Nest.new(email: 'user@email.com', password: 'mypassword', temperature_scale: :celsius)
+@sensibo = Sensibo::System.new('myapikey')
+@pods  = @sensibo.pods
 
-nestMode = nest.status["shared"][nest.device_id]["target_temperature_type"]
-nestTemp = nest.temp.round(0)
+#***** Get current state
 
-boilerCall = nest.status["shared"][nest.device_id]["hvac_alt_heat_state"]
-heatPumpCall = (nest.status['shared'][nest.device_id]['hvac_heater_state'] or nest.status['shared'][nest.device_id]['hvac_ac_state'])
+@nestMode = nest.status["shared"][nest.device_id]["target_temperature_type"]
+@nestTemp = nest.temp.round(0)
+@boilerCall = nest.status["shared"][nest.device_id]["hvac_alt_heat_state"]
+@heatPumpCall = (nest.status['shared'][nest.device_id]['hvac_heater_state'] or nest.status['shared'][nest.device_id]['hvac_ac_state'])
 
-if (heatPumpCall and !sensiboOn)
-  sensiboUpdate(true)
-end
+@sensiboOn = @pods[0].on
+@sensiboMode = @pods[0].mode
+@sensiboTemp = @pods[0].targetTemp
 
-if (sensiboOn and (sensiboTemp != nestTemp) or (sensiboMode != nestMode))
-  sensiboUpdate(true)
-end
+#***** Decide if an update is needed and request it
 
-if (boilerCall and sensiboOn)
-  sensiboUpdate(false)
-end
+def updateIfNeeded
 
-def sensiboUpdate (on)
-  case nestMode
-    when 'heat' podTemp = nestTemp + 2
-    when 'cool' podTemp = nestTemp
+  case @sensiboOn
+
+  when false
+    if @heatPumpCall
+      sensiboUpdate(true)
+      return 'swtich on'
+    else return 'still off'
+    end
+    
+  when true
+    if @boilerCall
+      sensiboUpdate(false)
+      return 'swtich off'
+    end
+    
+    if (@sensiboMode != @nestMode)
+      sensiboUpdate(true)
+      return 'mode change'
+    end
+    
+    if (@sensiboTemp != @nestTemp)
+      sensiboUpdate(true)
+      return 'temp change'
+    else return 'still on, no change'
+    end
   end
-  
-  sensibo.pods.each do |pod|
-    pod.setState(on, nestMode, sensiboFan, podTemp)
+end
+
+#***** Send update
+
+def sensiboUpdate (podOn)
+  sensiboFan = 'auto'
+     
+  @pods.each do |pod|
+    pod.setState(podOn, @nestMode, sensiboFan, @nestTemp)
   end
 end
+
+#***** Main
+
+puts updateIfNeeded
